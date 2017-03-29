fbb6b992963ad5199646bec3c53cbd8abf8d297d
egill <egill.ian1@gmail.com>
2017-03-29 18:49:00 +0000

    Start from scratch

--- asteroids.js
+++ asteroids.js
@@ -1,214 +1,209 @@
-let canvas;
-let gl;
+var canvas;
+var gl;
 
-let numVertices  = 36;
+var numVertices  = 6;
 
-let points = [];
-let colors = [];
+var program;
 
-let movement = false;   // Do we rotate?
-let spinX = 0;
-let spinY = 0;
-let origX;
-let origY;
+var pointsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
 
-let matrixLoc;
-let player;
+var texture;
+var texVegg;
+var texGolf;
+
+// Breytur fyrir hreyfingu áhorfanda
+var userXPos = 0.0;
+var userZPos = 2.0;
+var userIncr = 0.1;        // Size of forward/backward step
+var userAngle = 270.0;     // Direction of the user in degrees
+var userXDir = 0.0;        // X-coordinate of heading
+var userZDir = -1.0;       // Z-coordinate of heading
+
+var movement = false;
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var zDist = -5.0;
+
+var proLoc;
+var mvLoc;
+
+// Hnútar veggsins
+var vertices = [
+  vec4( -5.0,  0.0, 0.0, 1.0 ),
+  vec4(  5.0,  0.0, 0.0, 1.0 ),
+  vec4(  5.0,  1.0, 0.0, 1.0 ),
+  vec4(  5.0,  1.0, 0.0, 1.0 ),
+  vec4( -5.0,  1.0, 0.0, 1.0 ),
+  vec4( -5.0,  0.0, 0.0, 1.0 ),
+// Hnútar gólfsins (strax á eftir)
+  vec4( -5.0,  0.0, 10.0, 1.0 ),
+  vec4(  5.0,  0.0, 10.0, 1.0 ),
+  vec4(  5.0,  0.0,  0.0, 1.0 ),
+  vec4(  5.0,  0.0,  0.0, 1.0 ),
+  vec4( -5.0,  0.0,  0.0, 1.0 ),
+  vec4( -5.0,  0.0, 10.0, 1.0 )
+];
+
+// Mynsturhnit fyrir vegg
+var texCoords = [
+  vec2(  0.0, 0.0 ),
+  vec2( 10.0, 0.0 ),
+  vec2( 10.0, 1.0 ),
+  vec2( 10.0, 1.0 ),
+  vec2(  0.0, 1.0 ),
+  vec2(  0.0, 0.0 ),
+// Mynsturhnit fyrir gólf
+  vec2(  0.0,  0.0 ),
+  vec2( 10.0,  0.0 ),
+  vec2( 10.0, 10.0 ),
+  vec2( 10.0, 10.0 ),
+  vec2(  0.0, 10.0 ),
+  vec2(  0.0,  0.0 )
+];
+
 
 window.onload = function init() {
-  canvas = document.getElementById("gl-canvas");
+
+  canvas = document.getElementById( "gl-canvas" );
 
   gl = WebGLUtils.setupWebGL( canvas );
-  if (!gl)
-    alert( "WebGL isn't available" );
+  if ( !gl ) { alert( "WebGL isn't available" ); }
 
-  colorCube();
-
-  // initialize playable spaceship object
-  let spaceshipPoints = [
-    vec3(0.0, 0.0, 0.1),
-    vec3(-0.05, 0.0, 0.0),
-    vec3(0.05, 0.0, 0.0), // Bottom plate
-    vec3(0.0, 0.0, 0.1),
-    vec3(0.0, 0.05, 0.0), // Right side
-    vec3(0.0, 0.0, 0.1),
-    vec3(-0.05, 0.0, 0.0), // Left side
-    vec3(0.0, 0.05, 0.0),
-    vec3(0.05, 0.0, 0.0)
-  ];
-  let pos = vec3(0.0, 0.0, 0.0);
-  let dir = vec3(0.0, 0.0, 1.0);
-  player = new Ship(pos, dir, pos, spaceshipPoints);
-
-  gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+  gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
 
   gl.enable(gl.DEPTH_TEST);
 
+  //
   //  Load shaders and initialize attribute buffers
-  let program = initShaders(gl, "vertex-shader", "fragment-shader");
-  gl.useProgram(program);
+  //
+  program = initShaders( gl, "vertex-shader", "fragment-shader" );
+  gl.useProgram( program );
 
-  let cBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
-  let vColor = gl.getAttribLocation(program, "vColor");
-  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vColor);
+  var vPosition = gl.getAttribLocation( program, "vPosition" );
+  gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
 
-  let vBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+  var tBuffer = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW );
 
-  let vPosition = gl.getAttribLocation(program, "vPosition");
-  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition);
+  var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+  gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vTexCoord );
 
-  matrixLoc = gl.getUniformLocation(program, "rotation");
+  // Lesa inn og skilgreina mynstur fyrir vegg
+  var veggImage = document.getElementById("VeggImage");
+  texVegg = gl.createTexture();
+  gl.bindTexture( gl.TEXTURE_2D, texVegg );
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, veggImage );
+  gl.generateMipmap( gl.TEXTURE_2D );
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
 
-  // Event listeners for mouse
+  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+
+  // Lesa inn og skilgreina mynstur fyrir gólf
+  var golfImage = document.getElementById("GolfImage");
+  texGolf = gl.createTexture();
+  gl.bindTexture( gl.TEXTURE_2D, texGolf );
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, golfImage );
+  gl.generateMipmap( gl.TEXTURE_2D );
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
+  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+
+  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+
+
+  proLoc = gl.getUniformLocation( program, "projection" );
+  mvLoc = gl.getUniformLocation( program, "modelview" );
+
+  var proj = perspective( 50.0, 1.0, 0.2, 100.0 );
+  gl.uniformMatrix4fv(proLoc, false, flatten(proj));
+
+
+  //event listeners for mouse
   canvas.addEventListener("mousedown", function(e){
     movement = true;
-    origX = e.offsetX;
-    origY = e.offsetY;
-    e.preventDefault();     // Disable drag and drop
-  });
+    origX = e.clientX;
+  } );
 
   canvas.addEventListener("mouseup", function(e){
     movement = false;
-  });
+  } );
 
   canvas.addEventListener("mousemove", function(e){
     if(movement) {
-  	  spinY = ( spinY + (e.offsetX - origX) ) % 360;
-      spinX = ( spinX + (e.offsetY - origY) ) % 360;
-      origX = e.offsetX;
-      origY = e.offsetY;
+      userAngle += 0.4*(origX - e.clientX);
+      userAngle %= 360.0;
+      userXDir = Math.cos( radians(userAngle) );
+      userZDir = Math.sin( radians(userAngle) );
+      origX = e.clientX;
     }
-  });
+  } );
 
-  // Event listeners for keyboard
-  window.addEventListener("keydown", function(e){
-    key = e.keyCode;
-    if(key == 17){
-      console.log("Thrust engaged");
-    } else if(key == 32){
-      console.log("Shot fired");
-    } else if (key == 87){
-      console.log("Pitch up");
-    } else if (key == 83){
-      console.log("Pitch down");
-    } else if (key == 65){
-      console.log("Yaw left");
-    } else if (key == 68) {
-      console.log("Yaw right");
-    }
-  });
+  // Event listener for keyboard
+   window.addEventListener("keydown", function(e){
+     switch( e.keyCode ) {
+      case 87:	// w
+        userXPos += userIncr * userXDir;
+        userZPos += userIncr * userZDir;;
+        break;
+      case 83:	// s
+        userXPos -= userIncr * userXDir;
+        userZPos -= userIncr * userZDir;;
+        break;
+      case 65:	// a
+        userXPos += userIncr * userZDir;
+        userZPos -= userIncr * userXDir;;
+        break;
+      case 68:	// d
+        userXPos -= userIncr * userZDir;
+        userZPos += userIncr * userXDir;;
+        break;
+     }
+   }  );
+
+  // Event listener for mousewheel
+   window.addEventListener("mousewheel", function(e){
+     if( e.wheelDelta > 0.0 ) {
+       zDist += 0.2;
+     } else {
+       zDist -= 0.2;
+     }
+   }  );
+
 
   render();
+
 }
 
-// CLASSES
+var render = function(){
+  gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-class Ship {
-  constructor(position, direction, velocity, pointMatrix){
-    this.position = position;
-    this.direction = direction;
-    this.velocity = velocity;
-    this.pointMatrix = pointMatrix;
-  }
-}
+  // staðsetja áhorfanda og meðhöndla músarhreyfingu
+  var mv = lookAt( vec3(userXPos, 0.5, userZPos), vec3(userXPos+userXDir, 0.5, userZPos+userZDir), vec3(0.0, 1.0, 0.0 ) );
 
-// FUNCTIONS
+  gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
 
-function colorCube(){
-  quad( 1, 0, 3, 2 );
-  quad( 2, 3, 7, 6 );
-  quad( 3, 0, 4, 7 );
-  quad( 6, 5, 1, 2 );
-  quad( 4, 5, 6, 7 );
-  quad( 5, 4, 0, 1 );
-}
+  // Teikna vegg með mynstri
+  gl.bindTexture( gl.TEXTURE_2D, texVegg );
+  gl.drawArrays( gl.TRIANGLES, 0, numVertices );
 
-function quad(a, b, c, d){
-  let vertices = [
-    vec3( -0.5, -0.5,  0.5 ),
-    vec3( -0.5,  0.5,  0.5 ),
-    vec3(  0.5,  0.5,  0.5 ),
-    vec3(  0.5, -0.5,  0.5 ),
-    vec3( -0.5, -0.5, -0.5 ),
-    vec3( -0.5,  0.5, -0.5 ),
-    vec3(  0.5,  0.5, -0.5 ),
-    vec3(  0.5, -0.5, -0.5 )
-  ];
-
-  let vertexColors = [
-    [ 0.0, 0.0, 0.0, 1.0 ],  // black
-    [ 190.0/256.0, 78.0/256.0, 81.0/256.0, 1.0 ], // Carmine
-    [ 34.0/256.0, 170.0/256.0, 90.0/256.0, 1.0 ], // Emerald
-    [ 255.0/256.0, 147.0/256.0, 79.0/256.0, 1.0 ], // Dark orange
-    [ 147.0/256.0, 147.0/256.0, 147.0/256.0, 1.0 ], // Gray
-    [ 237.0/256.0, 203.0/256.0, 107.0/256.0, 1.0 ], // Yellow
-    [ 170.0/256.0, 72.0/256.0, 127.0/256.0, 1.0 ], // Magenta
-    [ 1.0, 1.0, 1.0, 1.0 ]   // white
-  ];
-  //vertex color assigned by the index of the vertex
-  let indices = [ a, b, c, a, c, d ];
-
-  for (let i = 0; i < indices.length; ++i) {
-    points.push(vertices[indices[i]]);
-    colors.push(vertexColors[a]);
-  }
-}
-
-// Returns matrix for transforming view og objects with roll, pitch
-// and yaw method. Variables roll, pitch and yaw are in degrees.
-function rpyView(roll, pitch, yaw, position){
-  let viewMatrix = mat4(
-    vec4(1, 0, 0, position[0]),
-    vec4(0, 1, 0, position[1]),
-    vec4(0, 0, 1, position[2]),
-    vec4(0, 0, 0, 1)
-  );
-
-  let transform = mult(rotateZ(roll), viewMatrix);
-  transform = mult(rotateX(pitch), transform);
-  transform = mult(rotateY(yaw), transform);
-  return transform;
-}
-
-// Moves all points by a set distance as dictated by the movement vector
-function movePoints(points, movement){
-  let xmove = movement[0];
-  let ymove = movement[1];
-  let zmove = movement[2];
-  for (var i = 0; i < points.length; i++) {
-    points[i][0] += xmove;
-    points[i][1] += ymove;
-    points[i][2] += zmove;
-  }
-}
-
-function render()
-{
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  let ctm = mat4();
-  ctm = mult(ctm, rotateX(spinX));
-  ctm = mult(ctm, rotateY(spinY));
-
-  movePoints(player.pointMatrix, [0.0, 0.0, 0.0]);
-  let shipPoints = player.pointMatrix;
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(shipPoints), gl.STATIC_DRAW);
-  gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, shipPoints.length);
-
-  /*
-  // Basic cube
-  gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
-  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
-  */
+  // Teikna gólf með mynstri
+  gl.bindTexture( gl.TEXTURE_2D, texGolf );
+  gl.drawArrays( gl.TRIANGLES, numVertices, numVertices );
 
   requestAnimFrame(render);
 }
---
+++ scrapped.js
@@ -0,0 +1,227 @@
+let canvas;
+let gl;
+
+let numVertices  = 36;
+
+let points = [];
+let colors = [];
+
+let movement = false;   // Do we rotate?
+let spinX = 0;
+let spinY = 0;
+let origX;
+let origY;
+
+let matrixLoc;
+let player;
+const degreesPerMovement = 10.0;
+
+window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
+
+  gl = WebGLUtils.setupWebGL( canvas );
+  if (!gl)
+    alert( "WebGL isn't available" );
+
+  colorCube();
+
+  // initialize playable spaceship object
+  let spaceshipPoints = [
+    vec3(0.0, 0.0, 0.1),
+    vec3(-0.05, 0.0, 0.0),
+    vec3(0.05, 0.0, 0.0), // Bottom plate
+    vec3(0.0, 0.0, 0.1),
+    vec3(0.0, 0.05, 0.0), // Right side
+    vec3(0.0, 0.0, 0.1),
+    vec3(-0.05, 0.0, 0.0), // Left side
+    vec3(0.0, 0.05, 0.0),
+    vec3(0.05, 0.0, 0.0)
+  ];
+  let ang = vec2(270.0, 0.0);
+  let pos = vec3(0.0, 0.0, 0.0);
+  let dir = vec3(0.0, 0.0, 1.0);
+  player = new Ship(ang, pos, dir, pos, spaceshipPoints);
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  //  Load shaders and initialize attribute buffers
+  let program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  let cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+  let vColor = gl.getAttribLocation(program, "vColor");
+  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+
+  let vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+  let vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  matrixLoc = gl.getUniformLocation(program, "rotation");
+
+  // Event listeners for mouse
+  canvas.addEventListener("mousedown", function(e){
+    movement = true;
+    origX = e.offsetX;
+    origY = e.offsetY;
+    e.preventDefault();     // Disable drag and drop
+  });
+
+  canvas.addEventListener("mouseup", function(e){
+    movement = false;
+  });
+
+  canvas.addEventListener("mousemove", function(e){
+    if(movement) {
+  	  spinY = ( spinY + (e.offsetX - origX) ) % 360;
+      spinX = ( spinX + (e.offsetY - origY) ) % 360;
+      origX = e.offsetX;
+      origY = e.offsetY;
+    }
+  });
+
+  // Event listeners for keyboard
+  window.addEventListener("keydown", function(e){
+    key = e.keyCode;
+    if(key == 17){
+      console.log("Thrust engaged");
+      player.position[0] += 0.1 * player.direction[0];
+      player.position[2] += 0.1 * player.direction[2];
+    } else if(key == 32){
+      console.log("Shot fired");
+    } else if (key == 87){
+      console.log("Pitch up");
+    } else if (key == 83){
+      console.log("Pitch down");
+    } else if (key == 65){
+      console.log("Yaw left");
+      player.angles[0] += degreesPerMovement;
+      player.direction[0] = Math.cos(radians(player.angles[0]));
+      player.direction[2] = Math.sin(radians(player.angles[0]));
+    } else if (key == 68) {
+      console.log("Yaw right");
+      player.angles[0] -= degreesPerMovement;
+      player.direction[0] = Math.cos(radians(player.angles[0]));
+      player.direction[2] = Math.sin(radians(player.angles[0]));
+    }
+  });
+
+  render();
+}
+
+// CLASSES
+
+class Ship {
+  constructor(angles, position, direction, velocity, pointMatrix){
+    this.angles = angles;
+    this.position = position;
+    this.direction = direction;
+    this.velocity = velocity;
+    this.pointMatrix = pointMatrix;
+  }
+}
+
+// FUNCTIONS
+
+function colorCube(){
+  quad( 1, 0, 3, 2 );
+  quad( 2, 3, 7, 6 );
+  quad( 3, 0, 4, 7 );
+  quad( 6, 5, 1, 2 );
+  quad( 4, 5, 6, 7 );
+  quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d){
+  let vertices = [
+    vec3( -0.5, -0.5,  0.5 ),
+    vec3( -0.5,  0.5,  0.5 ),
+    vec3(  0.5,  0.5,  0.5 ),
+    vec3(  0.5, -0.5,  0.5 ),
+    vec3( -0.5, -0.5, -0.5 ),
+    vec3( -0.5,  0.5, -0.5 ),
+    vec3(  0.5,  0.5, -0.5 ),
+    vec3(  0.5, -0.5, -0.5 )
+  ];
+
+  let vertexColors = [
+    [ 0.0, 0.0, 0.0, 1.0 ],  // black
+    [ 190.0/256.0, 78.0/256.0, 81.0/256.0, 1.0 ], // Carmine
+    [ 34.0/256.0, 170.0/256.0, 90.0/256.0, 1.0 ], // Emerald
+    [ 255.0/256.0, 147.0/256.0, 79.0/256.0, 1.0 ], // Dark orange
+    [ 147.0/256.0, 147.0/256.0, 147.0/256.0, 1.0 ], // Gray
+    [ 237.0/256.0, 203.0/256.0, 107.0/256.0, 1.0 ], // Yellow
+    [ 170.0/256.0, 72.0/256.0, 127.0/256.0, 1.0 ], // Magenta
+    [ 1.0, 1.0, 1.0, 1.0 ]   // white
+  ];
+  //vertex color assigned by the index of the vertex
+  let indices = [ a, b, c, a, c, d ];
+
+  for (let i = 0; i < indices.length; ++i) {
+    points.push(vertices[indices[i]]);
+    colors.push(vertexColors[a]);
+  }
+}
+
+// Returns matrix for transforming view og objects with roll, pitch
+// and yaw method. Variables roll, pitch and yaw are in degrees.
+function rpyView(roll, pitch, yaw, position){
+  let viewMatrix = mat4(
+    vec4(1, 0, 0, position[0]),
+    vec4(0, 1, 0, position[1]),
+    vec4(0, 0, 1, position[2]),
+    vec4(0, 0, 0, 1)
+  );
+
+  let transform = mult(rotateZ(roll), viewMatrix);
+  transform = mult(rotateX(pitch), transform);
+  transform = mult(rotateY(yaw), transform);
+  return transform;
+}
+
+// Moves all points by a set distance as dictated by the movement vector
+function movePoints(points, movement){
+  let xmove = movement[0];
+  let ymove = movement[1];
+  let zmove = movement[2];
+  for (var i = 0; i < points.length; i++) {
+    points[i][0] += xmove;
+    points[i][1] += ymove;
+    points[i][2] += zmove;
+  }
+}
+
+function render(){
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let ctm = mat4();
+
+  /*
+  movePoints(player.pointMatrix, [0.0, 0.0, 0.0]);
+  let shipPoints = player.pointMatrix;
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(shipPoints), gl.STATIC_DRAW);
+  gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, shipPoints.length);
+  */
+
+  let tmp = vec3(player.position[0] + player.direction[0], 0.0, player.position[2] + player.direction[2]);
+  ctm = lookAt(player.position, tmp, vec3(0.0, 1.0, 0.0));
+  //ctm = rpyView(player.direction[2], player.direction[1], player.direction[0], player.position)
+
+  // Basic cube
+  gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+
+
+  requestAnimFrame(render);
+}
