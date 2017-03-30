3fe6719e858a68d31df2490115a511c578aad2f3
egill <egill.ian1@gmail.com>
2017-03-30 00:04:22 +0000

    Basic movement

--- asteroids.js
+++ asteroids.js
@@ -1,69 +1,64 @@
-var canvas;
-var gl;
+let canvas;
+let gl;
 
-var numVertices  = 6;
+const numVertices  = 6;
 
-var program;
+let program;
 
-var pointsArray = [];
-var colorsArray = [];
-var texCoordsArray = [];
+let pointsArray = [];
+let colorsArray = [];
+let texCoordsArray = [];
 
-var texture;
-var texVegg;
-var texGolf;
+let texture;
+let wallTexture;
 
-// Breytur fyrir hreyfingu áhorfanda
-var userXPos = 0.0;
-var userZPos = 2.0;
-var userIncr = 0.1;        // Size of forward/backward step
-var userAngle = 270.0;     // Direction of the user in degrees
-var userXDir = 0.0;        // X-coordinate of heading
-var userZDir = -1.0;       // Z-coordinate of heading
+// Varibles for user view
+let userXPos = 0.0;
+let userYPos = 0.0;
+let userZPos = 0.0;
+const userIncr = 0.1; // Size of forward/backward step
+let userAngleX = 270.0; // X-direction of the user in degrees
+let userAngleY = 90.0; // Y-direction of the user in degrees
+let userXDir = 0.0; // X-coordinate of heading
+let userYDir = 0.0; // Y-coordinate of heading
+let userZDir = -1.0; // Z-coordinate of heading
+const degreesPerTurn = 5.0; // How many degrees are added/detracted for each button push
 
-var movement = false;
-var spinX = 0;
-var spinY = 0;
-var origX;
-var origY;
+let proLoc;
+let mvLoc;
 
-var zDist = -5.0;
-
-var proLoc;
-var mvLoc;
-
-// Hnútar veggsins
-var vertices = [
-  vec4( -5.0,  0.0, 0.0, 1.0 ),
-  vec4(  5.0,  0.0, 0.0, 1.0 ),
-  vec4(  5.0,  1.0, 0.0, 1.0 ),
-  vec4(  5.0,  1.0, 0.0, 1.0 ),
-  vec4( -5.0,  1.0, 0.0, 1.0 ),
-  vec4( -5.0,  0.0, 0.0, 1.0 ),
-// Hnútar gólfsins (strax á eftir)
-  vec4( -5.0,  0.0, 10.0, 1.0 ),
-  vec4(  5.0,  0.0, 10.0, 1.0 ),
-  vec4(  5.0,  0.0,  0.0, 1.0 ),
-  vec4(  5.0,  0.0,  0.0, 1.0 ),
-  vec4( -5.0,  0.0,  0.0, 1.0 ),
-  vec4( -5.0,  0.0, 10.0, 1.0 )
+// Vertices for boundary box
+let vertices = [
+  vec4( -10.0,  -10.0, -10.0, 1.0 ),
+  vec4(  10.0,  -10.0, -10.0, 1.0 ),
+  vec4(  10.0,  10.0, -10.0, 1.0 ),
+  vec4(  10.0,  10.0, -10.0, 1.0 ),
+  vec4( -10.0,  10.0, -10.0, 1.0 ),
+  vec4( -10.0,  -10.0, -10.0, 1.0 ),
+//
+  vec4( -10.0,  -10.0,  10.0, 1.0 ),
+  vec4(  10.0,  -10.0,  10.0, 1.0 ),
+  vec4(  10.0,  -10.0, -10.0, 1.0 ),
+  vec4(  10.0,  -10.0, -10.0, 1.0 ),
+  vec4( -10.0,  -10.0, -10.0, 1.0 ),
+  vec4( -10.0,  -10.0,  10.0, 1.0 )
 ];
 
-// Mynsturhnit fyrir vegg
-var texCoords = [
-  vec2(  0.0, 0.0 ),
-  vec2( 10.0, 0.0 ),
-  vec2( 10.0, 1.0 ),
-  vec2( 10.0, 1.0 ),
-  vec2(  0.0, 1.0 ),
-  vec2(  0.0, 0.0 ),
-// Mynsturhnit fyrir gólf
+// Texture co-ordinates for boundary box
+let texCoords = [
   vec2(  0.0,  0.0 ),
-  vec2( 10.0,  0.0 ),
-  vec2( 10.0, 10.0 ),
-  vec2( 10.0, 10.0 ),
-  vec2(  0.0, 10.0 ),
-  vec2(  0.0,  0.0 )
+  vec2( 5.0,  0.0 ),
+  vec2( 5.0, 5.0 ),
+  vec2( 5.0, 5.0 ),
+  vec2(  0.0, 5.0 ),
+  vec2(  0.0,  0.0 ),
+  //
+  vec2(  0.0,  0.0 ),
+  vec2( 5.0,  0.0 ),
+  vec2( 5.0, 5.0 ),
+  vec2( 5.0, 5.0 ),
+  vec2(  0.0, 5.0 ),
+  vec2(  0.0,  0.0 ),
 ];
 
 
@@ -83,26 +78,26 @@ window.onload = function init() {
   program = initShaders( gl, "vertex-shader", "fragment-shader" );
   gl.useProgram( program );
 
-  var vBuffer = gl.createBuffer();
+  let vBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
   gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
-  var vPosition = gl.getAttribLocation( program, "vPosition" );
+  let vPosition = gl.getAttribLocation( program, "vPosition" );
   gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
   gl.enableVertexAttribArray( vPosition );
 
-  var tBuffer = gl.createBuffer();
+  let tBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
   gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW );
 
-  var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+  let vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
   gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
   gl.enableVertexAttribArray( vTexCoord );
 
   // Lesa inn og skilgreina mynstur fyrir vegg
-  var veggImage = document.getElementById("VeggImage");
-  texVegg = gl.createTexture();
-  gl.bindTexture( gl.TEXTURE_2D, texVegg );
+  let veggImage = document.getElementById("VeggImage");
+  wallTexture = gl.createTexture();
+  gl.bindTexture( gl.TEXTURE_2D, wallTexture );
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, veggImage );
   gl.generateMipmap( gl.TEXTURE_2D );
@@ -111,27 +106,15 @@ window.onload = function init() {
 
   gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 
-  // Lesa inn og skilgreina mynstur fyrir gólf
-  var golfImage = document.getElementById("GolfImage");
-  texGolf = gl.createTexture();
-  gl.bindTexture( gl.TEXTURE_2D, texGolf );
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, golfImage );
-  gl.generateMipmap( gl.TEXTURE_2D );
-  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
-  gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
-
-  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
-
-
   proLoc = gl.getUniformLocation( program, "projection" );
   mvLoc = gl.getUniformLocation( program, "modelview" );
 
-  var proj = perspective( 50.0, 1.0, 0.2, 100.0 );
+  let proj = perspective( 50.0, 1.0, 0.2, 100.0 );
   gl.uniformMatrix4fv(proLoc, false, flatten(proj));
 
 
   // Event listeners for mouse
+  /*
   canvas.addEventListener("mousedown", function(e){
     movement = true;
     origX = e.clientX;
@@ -143,63 +126,78 @@ window.onload = function init() {
 
   canvas.addEventListener("mousemove", function(e){
     if(movement) {
-      userAngle += 0.4*(origX - e.clientX);
-      userAngle %= 360.0;
-      userXDir = Math.cos( radians(userAngle) );
-      userZDir = Math.sin( radians(userAngle) );
+      userAngleY += 0.4*(origX - e.clientX);
+      userAngleY %= 360.0;
+      userXDir = Math.cos(radians(userAngleX));
+      userYDir = Math.cos(radians(userAngleY));
+      userZDir = Math.sin(radians(userAngleX));
       origX = e.clientX;
     }
   });
+  */
 
   // Event listener for keyboard
    window.addEventListener("keydown", function(e){
-     switch( e.keyCode ) {
+     switch(e.keyCode) {
       case 87:	// w
-        userXPos += userIncr * userXDir;
-        userZPos += userIncr * userZDir;;
+        userAngleX += degreesPerTurn;
+        if(userAngleX == 360.0)
+          userAngleX += 0.5;
+        userAngleX %= 360.0;
+        userXDir = Math.sin(radians(userAngleX)) * Math.cos(radians(userAngleY));
+        userYDir = Math.cos(radians(userAngleX));
+        userZDir = Math.sin(radians(userAngleX)) * Math.sin(radians(userAngleY));
         break;
       case 83:	// s
-        userXPos -= userIncr * userXDir;
-        userZPos -= userIncr * userZDir;;
+        userAngleX -= degreesPerTurn;
+        if(userAngleX == 0.0)
+          userAngleX -= 0.5;
+        userAngleX %= 360.0;
+        userXDir = Math.sin(radians(userAngleX)) * Math.cos(radians(userAngleY));
+        userYDir = Math.cos(radians(userAngleX));
+        userZDir = Math.sin(radians(userAngleX)) * Math.sin(radians(userAngleY));
         break;
       case 65:	// a
-        userXPos += userIncr * userZDir;
-        userZPos -= userIncr * userXDir;;
+        userAngleY -= degreesPerTurn;
+        userAngleY %= 360.0;
+        userXDir = Math.sin(radians(userAngleX)) * Math.cos(radians(userAngleY));
+        userYDir = Math.cos(radians(userAngleX));
+        userZDir = Math.sin(radians(userAngleX)) * Math.sin(radians(userAngleY));
         break;
       case 68:	// d
-        userXPos -= userIncr * userZDir;
-        userZPos += userIncr * userXDir;;
+        userAngleY += degreesPerTurn;
+        userAngleY %= 360.0;
+        userXDir = Math.sin(radians(userAngleX)) * Math.cos(radians(userAngleY));
+        userYDir = Math.cos(radians(userAngleX));
+        userZDir = Math.sin(radians(userAngleX)) * Math.sin(radians(userAngleY));
         break;
-     }
-   });
-
-  // Event listener for mousewheel
-   window.addEventListener("mousewheel", function(e){
-     if( e.wheelDelta > 0.0 ) {
-       zDist += 0.2;
-     } else {
-       zDist -= 0.2;
+      case 74:  // ctrl
+        userXPos += userIncr * userXDir;
+        userYPos += userIncr * userYDir
+        userZPos += userIncr * userZDir;
+        break;
      }
    });
 
   render();
 }
 
-var render = function(){
+let render = function(){
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // staðsetja áhorfanda og meðhöndla músarhreyfingu
-  var mv = lookAt( vec3(userXPos, 0.5, userZPos), vec3(userXPos+userXDir, 0.5, userZPos+userZDir), vec3(0.0, 1.0, 0.0 ) );
+  // Positon viewer
+  let mv = lookAt(vec3(userXPos, userYPos, userZPos),
+  vec3(userXPos+userXDir, userYPos+userYDir, userZPos+userZDir),
+  vec3(0.0, 1.0, 0.0 ));
 
   gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
 
-  // Teikna vegg með mynstri
-  gl.bindTexture( gl.TEXTURE_2D, texVegg );
-  gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+  gl.bindTexture(gl.TEXTURE_2D, wallTexture);
+  for (var i = 0; (i*numVertices) < vertices.length; i++) {
+    gl.drawArrays(gl.TRIANGLES, i*numVertices, numVertices);
+  }
 
-  // Teikna gólf með mynstri
-  gl.bindTexture( gl.TEXTURE_2D, texGolf );
-  gl.drawArrays( gl.TRIANGLES, numVertices, numVertices );
+  //gl.drawArrays(gl.TRIANGLES, numVertices, numVertices);
 
   requestAnimFrame(render);
 }
